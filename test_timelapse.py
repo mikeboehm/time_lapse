bac87c8f17e90fd2b70e06a69d04b6954541d8bf
Mike Boehm <mdboehm@gmail.com>
2014-06-01 15:46:52 +0000

    added files

---
+++ test_timelapse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import timelapse
+import unittest
+import datetime
+import time
+import os
+
+os.system('clear')
+
+class TestTimelapse(unittest.TestCase):
+
+	def setUp(self):
+		self.timelapse = Timelapse()
+	
+if __name__ == '__main__':
+	unittest.main()
---
+++ timelapse.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import datetime
+
+class TimeLapse(object):
+	def __init__(self):
+		pass
+		
+		
